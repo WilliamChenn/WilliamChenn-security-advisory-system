e6e84aa53e074d67949e211ee6eaf48c1bcb62fd
Katherine Yu <katherine.l.yu@duke.edu>
2024-05-31 12:53:42 -0400

    Update 7 files
    - /front-end/src/App.js
    - /front-end/src/Home.css
    - /front-end/src/Home.js
    - /front-end/src/components/App.js
    - /front-end/src/components/Home.css
    - /front-end/src/components/Home.js
    - /front-end/src/components/App.css

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Vulnerabilities from './components/Vulnerabilities';
-import './App.css';
+import './Home.css';
 
 function App() {
   return (
