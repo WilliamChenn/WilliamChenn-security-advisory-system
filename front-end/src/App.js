a225d0d283feafc0aa62a7037841955d8f65c824
Brian Kim <bdk21@duke.edu>
2024-05-31 10:10:48 -0400

    added stuff to main page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import duke from './images/duke.png';
 import './App.css';
 
@@ -5,16 +6,40 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img
-          src={duke}
-          className="Duke-logo"
-          alt="logo"
-        />
-
+        <img src={duke} className="Duke-logo" alt="logo" />
         <div className="title">Duke IT Security Advisory Dashboard</div>
-        <div className="subtitle">Duke’s database for open source vulnerabilities and cloud misconfigurations</div>
+        <div className="subtitle">
+          Duke’s database for open source vulnerabilities and cloud misconfigurations
+        </div>
         <a href="#" className="button">Get Started</a>
       </header>
+      
+      
+      
+      <div className="content">
+        <h2>Vulnerabilities in the last week</h2>
+        <div className="cards">
+          <div className="card">
+            <div className="score" style={{ color: '#ffa500' }}>6.5</div>
+            <div className="notification-name">Notification Name</div>
+            <p> Stuff</p>
+            <a href="#" className="learn-more">Learn More</a>
+          </div>
+          <div className="card">
+            <div className="score" style={{ color: '#ff7f50' }}>5.3</div>
+            <div className="notification-name">Notification Name</div>
+            <p> Stuff </p>
+            <a href="#" className="learn-more">Learn More</a>
+          </div>
+          <div className="card">
+            <div className="score" style={{ color: '#ff4500' }}>9.3</div>
+            <div className="notification-name">Notification Name</div>
+            <p> Stuff </p>
+            <a href="#" className="learn-more">Learn More</a>
+          </div>
+        </div>
+        <a href="#" className="view-all">View all</a>
+      </div>
     </div>
   );
 }
