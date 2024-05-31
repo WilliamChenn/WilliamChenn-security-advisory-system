50451184f62d041aaf0449d269c1f42c52bf46c2
Katherine Yu <katherine.l.yu@duke.edu>
2024-05-31 15:36:17 -0400

    severity try 2.0

--- front-end/src/components/Vulnerabilities.js
+++ front-end/src/components/Vulnerabilities.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './Vulnerabilities.css';
 
 const data = [
-    { title:"Improper Input Validation", date:"May 22 2024", severity:"Low", cve:"2024-29946"},
-    { title:"Authentication Bypass", date:"February 1 2024", severity:"Critical", cve:"2024-21222"},
-    { title:"Uncontrolled Resource Consumption", date:"November 15 2023", severity:"Low", cve:"2024-29946"},
+    { title: "Improper Input Validation", date: "May 22 2024", severity: "Low", cve: "2024-29946" },
+    { title: "Authentication Bypass", date: "February 1 2024", severity: "Critical", cve: "2024-21222" },
+    { title: "Uncontrolled Resource Consumption", date: "November 15 2023", severity: "Low", cve: "2024-29946" },
 ]
 
 function getSeverityClass(severity) {
@@ -25,31 +25,35 @@ function getSeverityClass(severity) {
 function Vulnerabilities() {
     return (
         <div className="Vulnerabilities">
-                <header className="Vulnerabilities-header">
-                    <div className="title">FIND OUT IF YOU HAVE VULNERABILITIES THAT PUT YOU AT RISK</div>
-                </header>
+            <header className="Vulnerabilities-header">
+                <div className="title">FIND OUT IF YOU HAVE VULNERABILITIES THAT PUT YOU AT RISK</div>
+            </header>
 
             <div className="Table">
                 <table>
-                    <tr>
-                        <th>Title</th>
-                        <th>Date</th>
-                        <th>Severity</th>
-                        <th>CVE</th>
-                    </tr>
-                    {data.map((val, key) => {
-                        return (
-                            <tr key = {key}>
-                                <td>{val.title}</td>
-                                <td>{val.date}</td>
-                                <td className={getSeverityClass(val.severity)}>{val.severity}</td>
-                                <td>{val.cve}</td>
-                            </tr>
-                        )
-                    })}
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Date</th>
+                            <th>Severity</th>
+                            <th>CVE</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.map((val, key) => {
+                            return (
+                                <tr key={key}>
+                                    <td>{val.title}</td>
+                                    <td>{val.date}</td>
+                                    <td className={getSeverityClass}>{val.severity}</td>
+                                    <td>{val.cve}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
                 </table>
             </div>
-        </div>
+        </div >
     );
 }
 
