484e2298f02340d015007ad5522b4e61c39b9f94
Katherine Yu <katherine.l.yu@duke.edu>
2024-06-05 15:50:20 -0400

    Merge branch 'main' of gitlab.oit.duke.edu:codeplus/security-advisory-system

--- front-end/src/components/Vulnerabilities.js
+++ front-end/src/components/Vulnerabilities.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import './Vulnerabilities.css';
+import Tooltip from '@mui/material/Tooltip';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min'; // This includes Popper.js
+
+
 
 const data = [
-    { cve: "2024-29946", summary: "In onCreate of WifiDialogActivity.java, there is a possible way to bypass the DISALLOW_ADD_WIFI_CONFIG restriction due to a missing permission check.", publishDate: "2024-05-07", updateDate: "2024-05-29", product: "Roothub", cvss: 2.1, epss: "0.05"},
-    { cve: "2024-28888", summary: "Roothub v2.6 was discovered to contain a SQL injection vulnerability via the 's' parameter in the search() function.", publishDate: "2024-03-12", updateDate: "2024-04-22", product: "Roothub", cvss: 7.1, epss: "0.02"},
-    { cve: "2024-28753", summary: "LuckyFrameWeb v3.5.2 was discovered to contain an arbitrary read vulnerability via the fileDownload method in class com.luckyframe.project.common.CommonController.", publishDate: "2023-12-15", updateDate: "2024-02-15", product: "Luckyframe", cvss: 4.6, epss: "96.2"},
+    { cve: "2024-29946", summary: "In onCreate of WifiDialogActivity.java, there is a possible way to bypass the DISALLOW_ADD_WIFI_CONFIG restriction due to a missing permission check.", publishDate: "2024-05-07", updateDate: "2024-05-29", product: "Roothub", cvss: 2.1, epss: "0.05" },
+    { cve: "2024-28888", summary: "Roothub v2.6 was discovered to contain a SQL injection vulnerability via the 's' parameter in the search() function.", publishDate: "2024-03-12", updateDate: "2024-04-22", product: "Roothub", cvss: 7.1, epss: "0.02" },
+    { cve: "2024-28753", summary: "LuckyFrameWeb v3.5.2 was discovered to contain an arbitrary read vulnerability via the fileDownload method in class com.luckyframe.project.common.CommonController.", publishDate: "2023-12-15", updateDate: "2024-02-15", product: "Luckyframe", cvss: 4.6, epss: "96.2" },
 ];
 
 function getSeverityClass(cvss) {
     if (cvss < 4) {
-        return "low-severity";
+        return "Low-severity";
     }
     else if (cvss >= 4 && cvss < 7) {
-        return "medium-severity";
+        return "Medium-severity";
     }
     else if (cvss >= 7 && cvss < 9) {
-        return "high-severity";
+        return "High-severity";
     }
     else if (cvss >= 9 && cvss <= 10) {
-        return "critical-severity";
+        return "Critical-severity";
     }
     else {
         return "n-a";
@@ -27,16 +32,16 @@ function getSeverityClass(cvss) {
 
 function getEpssClass(epss) {
     if (epss < 0.05) {
-        return "low-severity";
+        return "Low-probability";
     }
     else if (epss >= 0.05 && epss < 1) {
-        return "medium-severity";
+        return "Medium-probability";
     }
     else if (epss >= 1 && epss < 10) {
-        return "high-severity";
+        return "High-probability";
     }
     else if (epss >= 10 && epss <= 100) {
-        return "critical-severity";
+        return "Very-high-probability";
     }
     else {
         return "n-a";
@@ -70,8 +75,20 @@ function Vulnerabilities() {
                                 <td>{val.publishDate}</td>
                                 <td>{val.updateDate}</td>
                                 <td>{val.product}</td>
-                                <td className="severityCell">CVSS:<button id= {getSeverityClass(val.cvss)}>{val.cvss}</button></td>
-                                <td className="severityCell">EPSS: <button id= {getEpssClass(val.epss)}>{val.epss}%</button></td>
+                                <td className="severityCell">
+                                    <Tooltip title="Rates severity of security vulnerabilities on a scale of 1-10" placement="top" arrow>
+                                        <span className="tooltip-target">CVSS:</span></Tooltip>
+                                    <button id={getSeverityClass(val.cvss)}>
+                                        <Tooltip title= {getSeverityClass(val.cvss)} placement="top" arrow>
+                                            <span className="tooltip-target">{val.cvss}%</span></Tooltip>
+                                        </button></td>
+                                <td className="severityCell">
+                                    <Tooltip title="Likelihood of the vulnerability being exploited in the wild within the next 30 days" placement="top" arrow>
+                                        <span className="tooltip-target">EPSS:</span> </Tooltip>
+                                    <button id={getEpssClass(val.epss)}>
+                                        <Tooltip title= {getEpssClass(val.epss)} placement="top" arrow>
+                                            <span className="tooltip-target">{val.epss}%</span></Tooltip>
+                                        </button></td>
                             </tr>
                         ))}
                     </tbody>
