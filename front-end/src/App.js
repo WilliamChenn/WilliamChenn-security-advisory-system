f0ab476b16a15d62ad8f51275a326f6d68e669d2
Kelly Shahu <ks756@duke.edu>
2024-06-05 11:15:11 -0400

    Three different LearnMore pages

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,20 +4,23 @@ import Home from './components/Home';
 import Vulnerabilities from './components/Vulnerabilities';
 import './App.css';
 import TableComponent from './TableComponent';
-import LearnMore from './LearnMore';
+import LearnMore1 from './components/LearnMore1';
+import LearnMore2 from './components/LearnMore2';
+import LearnMore3 from './components/LearnMore3';
 
 function App() {
   return (
-
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/vulnerabilities" element={<Vulnerabilities />} />
-        <Route path="/learn-more" element={<LearnMore />} />
+        <Route path="/learn-more/1" element={<LearnMore1 />} />
+        <Route path="/learn-more/2" element={<LearnMore2 />} />
+        <Route path="/learn-more/3" element={<LearnMore3 />} />
       </Routes>
     </Router>
-
   );
 }
 
 export default App;
+
---
+++ front-end/src/components/LearnMore3.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const LearnMore3 = () => {
+  return (
+    <div>
+      <h1>Vulnerability Title /CVE 3</h1>
+      <h2>Overview</h2>
+      <p>A description of the vulnerability for the third item.</p>
+      <h3>Affected:</h3>
+      <p>Affected versions of this package are vulnerable to yet another issue.</p>
+      <h4>Remediation</h4>
+      <p>Upgrade to the latest version to address this vulnerability.</p>
+      <img className="rounded-image" src="https://yet-another-image-url.jpg" alt="Description of the image"/> 
+    </div>
+  );
+};
+
+export default LearnMore3;
