fd191f38a8d805fe6d1bb23c577a4dd331ed3b49
William Chen <william.chen@duke.edu>
2024-06-07 15:39:34 -0400

    stuff

--- back-end/app/controllers/api/v1/cves_controller.rb
+++ back-end/app/controllers/api/v1/cves_controller.rb
@@ -5,12 +5,12 @@ module Api
           cves = CVE.includes(:vendor).order(publish_date: :desc)
           render json: CVESerializer.new(cves, include: [:vendor]).serialized_json
         end
-  
+        #/api/v1/cves/critical
         def critical
           cves = CVE.where('max_cvss_base_score >= ?', 7.0).order(publish_date: :desc).limit(5)
           render json: CVESerializer.new(cves, include: [:vendor]).serialized_json
         end
-  
+        #/api/v1/cves/recent
         def recent
           cves = CVE.order(publish_date: :desc).limit(5)
           render json: CVESerializer.new(cves, include: [:vendor]).serialized_json
