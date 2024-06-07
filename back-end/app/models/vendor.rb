2b9a41727094cb5bc8044b3282f205a5e148607c
William Chen <william.chen@duke.edu>
2024-06-07 11:59:03 -0400

    updating main

--- back-end/app/models/vendor.rb
+++ back-end/app/models/vendor.rb
@@ -1,3 +1,7 @@
 class Vendor < ApplicationRecord
     has_many :cves, dependent: :destroy
+
+    #The dependent: :destroy option in a has_many association in Rails specifies that 
+    #when the parent record (in this case, a Vendor) is destroyed, 
+    #all associated child records (in this case, Cve records) should also be destroyed.
 end
