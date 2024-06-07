faa829182a77d7edb4f3011fcf262201f5761c93
Katherine Yu <katherine.l.yu@duke.edu>
2024-06-07 16:19:12 -0400

    filtered table

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 import LearnMore1 from './pages/LearnMore1';
 import LearnMore2 from './pages/LearnMore2';
 import LearnMore3 from './pages/LearnMore3';
-import BasicTable from './pages/Table/BasicTable';
+import SortingTable from './pages/Table/SortingTable';
 // import { Table } from '@mui/material';
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
         <Route path="/learn-more/1" element={<LearnMore1 />} />
         <Route path="/learn-more/2" element={<LearnMore2 />} />
         <Route path="/learn-more/3" element={<LearnMore3 />} />
-        <Route path="/table" element={<BasicTable />} />
+        <Route path="/table" element={<SortingTable />} />
       </Routes>
     </Router>
   );
