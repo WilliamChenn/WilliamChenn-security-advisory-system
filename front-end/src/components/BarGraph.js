a21e6f9a1fa15e6e407d7080f0ade165fb073966
Kelly Shahu <ks756@duke.edu>
2024-06-11 15:32:34 -0400

    changes

--- front-end/src/components/BarGraph.js
+++ front-end/src/components/BarGraph.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Cell, Label } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Cell, ResponsiveContainer, LabelList, Label } from 'recharts';
 import './BarGraph.css';
 
 const BarGraph = () => {
@@ -15,9 +15,7 @@ const BarGraph = () => {
         return response.json();
       })
       .then(parsedData => {
-        console.log("Fetched Data:", parsedData); // Debugging: Check fetched data
-
-        // Extract and format data with colors
+        console.log("Fetched Data:", parsedData);
         const scoreRanges = {
           '0-1': { count: 0, color: '#00FF00' }, // Green
           '1-2': { count: 0, color: '#7FFF00' }, // Light Green
@@ -60,21 +58,32 @@ const BarGraph = () => {
       .catch(error => console.error('Error fetching data:', error));
   }, []);
 
+  const CustomLabel = ({ x, y, width, value }) => (
+    <text x={x + width + 5} y={y + 5} fill="blue" dy={10} textAnchor="start">
+      {value}
+    </text>
+  );
+
   return (
-    <BarChart width={800} height={400} data={data} className="bar-chart">
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="scoreRange">
-        <Label value="CVSS Score" offset={-5} position="insideBottom" />
-      </XAxis>
-      <YAxis />
-      <Tooltip />
-      <Bar dataKey="count">
-        {data.map((entry, index) => (
-          <Cell key={`cell-${index}`} fill={entry.fill} />
-        ))}
-      </Bar>
-    </BarChart>
+    <div className="bar-chart">
+      <ResponsiveContainer width="100%" height={400}>
+        <BarChart layout="vertical" data={data} margin={{ top: 40, left: 80 }}>
+          <XAxis type="number" hide />
+          <YAxis type="category" dataKey="scoreRange">
+            <Label value="CVSS Score" position="insideTop" offset={-17} />
+          </YAxis>
+          <Tooltip />
+          <Bar dataKey="count" barSize={20}>
+            {data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={entry.fill} />
+            ))}
+            <LabelList dataKey="count" position="right" content={CustomLabel} />
+          </Bar>
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
   );
 };
 
 export default BarGraph;
+
---
+++ front-end/src/pages/Table/columns.js
@@ -0,0 +1,90 @@
+import { convertLength } from "@mui/material/styles/cssUtils";
+import Tooltip from '@mui/material/Tooltip';
+
+const getSeverityClass = (cvss) => {
+    if (cvss < 4) {
+        return "Low-severity";
+    }
+    else if (cvss >= 4 && cvss < 7) {
+        return "Medium-severity";
+    }
+    else if (cvss >= 7 && cvss < 9) {
+        return "High-severity";
+    }
+    else if (cvss >= 9 && cvss <= 10) {
+        return "Critical-severity";
+    }
+    else {
+        return "n-a";
+    }
+};
+
+const getEpssClass = (epss) => {
+    if (epss < 0.05) {
+        return "Low-probability";
+    }
+    else if (epss >= 0.05 && epss < 1) {
+        return "Medium-probability";
+    }
+    else if (epss >= 1 && epss < 10) {
+        return "High-probability";
+    }
+    else if (epss >= 10 && epss <= 100) {
+        return "Very-high-probability";
+    }
+
+};
+
+export const COLUMNS = [
+    {
+        Header: 'CVE',
+        accessor: 'cve',
+        Cell: ({ value }) => (
+            <a href="#">{value}</a>
+        )
+    },
+    {
+        Header: 'Summary',
+        accessor: 'summary',
+    },
+    {
+        Header: 'Published',
+        accessor: 'publishDate'
+    },
+    {
+        Header: 'Updated',
+        accessor: 'updateDate'
+    },
+    {
+        Header: 'Affected Product',
+        accessor: 'product'
+    },
+    {
+        Header: 'Criticality',
+        accessor: row => `${row.cvss} - ${row.epss}`, // Custom accessor to combine cvss and epss
+        Cell: ({ row }) => {
+            const cvss = row.original.cvss;
+            const epss = row.original.epss;
+            return (
+                <div className="severityCell">
+                    <Tooltip title="Severity of security vulnerabilities (scale of 1-10)" placement="top" arrow>
+                        <span className="tooltip-target">CVSS:</span>
+                    </Tooltip>
+                    <button id={getSeverityClass(cvss)}>
+                        <Tooltip title={getSeverityClass(cvss)} placement="top" arrow>
+                            <span className="tooltip-target">{cvss}</span>
+                        </Tooltip>
+                    </button>
+                    <Tooltip title="Likelihood of the vulnerability being exploited in the wild within the next 30 days" placement="top" arrow>
+                        <span className="tooltip-target">EPSS:</span>
+                    </Tooltip>
+                    <button id={getEpssClass(epss)}>
+                        <Tooltip title={getEpssClass(epss)} placement="top" arrow>
+                            <span className="tooltip-target">{epss}%</span>
+                        </Tooltip>
+                    </button>
+                </div>
+            );
+        }
+    }
+];
