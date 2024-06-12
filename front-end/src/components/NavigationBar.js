9125e73778fed5cc359df5810abde66e8815fff4
Brian Kim <bdk21@duke.edu>
2024-06-12 10:19:56 -0400

    vendor logos

--- front-end/src/components/NavigationBar.js
+++ front-end/src/components/NavigationBar.js
@@ -27,14 +27,8 @@ function NavigationBar() {
               <Nav className="me-auto">
                 <Nav.Link as={Link} to="/">Home</Nav.Link>
                 <Nav.Link as={Link} to="/about">About</Nav.Link>
+                <Nav.Link as={Link} to="/table">Search</Nav.Link>
                 <Nav.Link as={Link} to="/search">Search</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                  <NavDropdown.Item as={Link} to="/action1">Action</NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/action2">Another action</NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/something">Something</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item as={Link} to="/separated-link">Separated link</NavDropdown.Item>
-                </NavDropdown>
               </Nav>
             </Navbar.Collapse>
           </Container>
