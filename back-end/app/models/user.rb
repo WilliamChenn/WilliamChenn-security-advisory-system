59fd1f0909103dac0710c740eba965abbe544502
William Chen <william.chen@duke.edu>
2024-06-20 14:56:03 -0400

    user auth

--- back-end/app/models/user.rb
+++ back-end/app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-    has_many :users_vendors
-    has_many :vendors, through: :users_vendors
-  end
+    self.table_name = 'users'  # Explicitly set the table name if it's not following Rails conventions
   
+    validates :Uid, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    validates :auth_token, uniqueness: true
+  
+    # Add any additional validations or associations if needed
+  end
