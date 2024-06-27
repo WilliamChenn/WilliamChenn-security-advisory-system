84026d1a1dc44307962c6bcf8b9e1ae556e1fa01
Brian Kim <bdk21@duke.edu>
2024-06-27 15:35:34 -0400

    WORKING

--- back-end/app/models/users_vendor.rb
+++
@@ -1,5 +0,0 @@
-class UsersVendor < ApplicationRecord
-    belongs_to :user
-    belongs_to :vendor
-  end
-  
---
+++ back-end/db/migrate/20240627181220_add_unique_constraint_to_users_uid.rb
@@ -0,0 +1,7 @@
+class AddUniqueConstraintToUsersUid < ActiveRecord::Migration[7.1]
+    def change
+      add_index :users, :uid, unique: true
+    end
+  end
+  
+  
