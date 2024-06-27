69e4f5a9b8022741987d8968a852ea81e39e7ed9
Brian Kim <bdk21@duke.edu>
2024-06-27 14:53:17 -0400

    remove cache

--- back-end/db/schema.rb
+++ back-end/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_25_192912) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_27_181721) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -73,7 +73,28 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_25_192912) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "source"
+    t.string "remediation_url"
     t.index ["vendor_id"], name: "index_cves_on_vendor_id"
+  end
+
+  create_table "kevs", force: :cascade do |t|
+    t.string "cve_id"
+    t.string "vendor_project"
+    t.string "product"
+    t.string "vulnerability_name"
+    t.date "date_added"
+    t.text "short_description"
+    t.text "required_action"
+    t.date "due_date"
+    t.string "known_ransomware_campaign_use"
+    t.text "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "uids_vendors", id: false, force: :cascade do |t|
+    t.bigint "uid_id", null: false
+    t.bigint "vendor_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -83,11 +104,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_25_192912) do
     t.string "auth_token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "users_vendors", id: false, force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "vendor_id", null: false
+    t.index ["uid"], name: "index_users_on_uid", unique: true
   end
 
   create_table "vendors", force: :cascade do |t|
