7d0f98810b3468c444c9467b41f0c4d30a804e9e
William Chen <william.chen@duke.edu>
2024-07-01 13:41:36 -0400

    token updated

--- back-end/db/schema.rb
+++ back-end/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_27_233731) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_01_201548) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -74,6 +74,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_27_233731) do
     t.datetime "updated_at", null: false
     t.string "source"
     t.string "remediation_url"
+    t.string "vulnerability_name"
+    t.string "product"
+    t.boolean "known_ransomware_campaign_use"
     t.index ["vendor_id"], name: "index_cves_on_vendor_id"
   end
 
