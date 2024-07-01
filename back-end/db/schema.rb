6dcb0de1b5c27c9528540268548be65fc9659e8f
Brian Kim <bdk21@duke.edu>
2024-07-01 15:10:33 -0400

    schema

--- back-end/db/schema.rb
+++ back-end/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_01_201548) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_01_183222) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -70,13 +70,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_01_201548) do
     t.boolean "is_denial_of_service"
     t.boolean "is_information_leak"
     t.boolean "is_used_for_ransomware"
+    t.string "source"
+    t.string "product"
+    t.string "vulnerability_name"
+    t.boolean "known_ransomware_campaign_use"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "source"
-    t.string "remediation_url"
-    t.string "vulnerability_name"
-    t.string "product"
-    t.boolean "known_ransomware_campaign_use"
     t.index ["vendor_id"], name: "index_cves_on_vendor_id"
   end
 
