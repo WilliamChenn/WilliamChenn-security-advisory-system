194a5c45ff9afd0baa59dd49fdd6de3c660c8bd0
Kelly Shahu <ks756@duke.edu>
2024-07-01 15:58:54 -0400

    changes

---
+++ back-end/db/migrate/20240625192912_add_remediation_url_to_vendors.rb
@@ -0,0 +1,6 @@
+class AddRemediationUrlToVendors < ActiveRecord::Migration[7.1]
+    def change
+      add_column :vendors, :remediation_url, :string
+    end
+  end
+  
---
+++ back-end/db/migrate/20240625192913_add_remediation_url_to_cves.rb
@@ -0,0 +1,6 @@
+class AddRemediationUrlToCves < ActiveRecord::Migration[7.1]
+    def change
+      add_column :cves, :remediation_url, :string
+    end
+  end
+  
