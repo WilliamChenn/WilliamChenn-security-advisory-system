b9f40c35621de2e96115f1a91989420f2cbe3007
Kelly Shahu <ks756@duke.edu>
2024-07-10 10:43:20 -0400

    change

--- back-end/db/schema.rb
+++ back-end/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_08_155400) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_09_190059) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -101,6 +101,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_08_155400) do
     t.bigint "vendor_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "frequency"
     t.index ["user_id"], name: "index_user_notification_vendors_on_user_id"
     t.index ["vendor_id"], name: "index_user_notification_vendors_on_vendor_id"
   end
